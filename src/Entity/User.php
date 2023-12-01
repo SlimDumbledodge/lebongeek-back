0b844c4b1f35a1f09cd829cf84be6d755bc02708
matthieulefloch <matthfloch@gmail.com>
2023-12-01 09:40:46 +0100

    s

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,8 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  */
@@ -21,6 +23,7 @@ class User
 
     /**
      * @ORM\Column(type="string", length=64)
+     * @Assert\NotNull(message="Ne doit pas etre null")
      */
     private $username;
 
