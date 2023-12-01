7611c621ba1934af519bd545ac8c4b0b5beca7a6
matthieulefloch <matthfloch@gmail.com>
2023-12-01 13:25:07 +0100

    test

--- src/Entity/Ad.php
+++ src/Entity/Ad.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use App\Repository\AdRepository;
 use Doctrine\ORM\Mapping as ORM;
 
-use Symfony\Component\Validator\Constraints as Assert;
+
 
 /**
  * @ORM\Entity(repositoryClass=AdRepository::class)
